c54b605f5ac01e3891263fdd9bc23378b0de28b7
Andrew Peter Fleischer <andy.fleischer08@gmail.com>
2023-02-24 22:05:14 -0800

    Added query ranking

--- assignment3/search.py
+++ assignment3/search.py
@@ -1,17 +1,49 @@
 import pickle
 import itertools
 
+"""
+search_result
+{(url1, 3), {url2, 4}}
+
+urls
+{url1, url2}
+
+new
+{(url2, 5), (url6, 2)...}
+
+search_result
+{(url2, 9)}
+
+urls
+{url2}
+
+"""
+
 def search_request(request, index):
-    if(index.get(request[0]) == None):
+    if not request:
         return []
-    search_result = [x[0] for x in index.get(request[0])] #get list of urls
-    for i in range(1,len(request)):
-        if index.get(request[i]) != None:
-            temp = [x[0] for x in index.get(request[i])] #get list of urls
-            search_result = set(search_result) & set(temp)
-        else:
+
+    if index.get(request[0]) == None:
+        return []
+
+    search_result = set(index.get(request[0]))
+    
+    for token in request[1:]:
+        if index.get(token) == None:
             return []
-    return list(search_result)
+
+        new_results = set(index.get(token))
+        new_search_result = set()
+
+        for new_result in new_results: # not at all optimized
+            for result in search_result: 
+                if new_result[0] == result[0]: # both tokens in the same url, add their count
+                    new_search_result.add((new_result[0], new_result[1] + result[1]))
+        
+        search_result = new_search_result
+    
+    return [result[0] for result in sorted(search_result, key=lambda x:x[1], reverse=True)]
+
 
 def get_index():
     with open('index.pickle', 'rb') as file:
@@ -19,12 +51,14 @@ def get_index():
     
 
 def create_temp_index(): 
-    return { "a":[("url1",10)], "b":[("url2",4)], "c":[("url1",9),("url2",3)], "d": [("url1",1)]}
+    return { "a":[("url1",10),("url2", 20),("url3", 5)], "b":[("url2",4)], "c":[("url1",9),("url2",3)], "d": [("url1",1)]}
 
 
 if __name__ == '__main__':
-    # index = get_index()
-    index = create_temp_index()
+    print("Loading index...")
+    index = get_index()
+    print("Loaded!")
+    # index = create_temp_index()
     
     while True:
         request = input('Query: ')
@@ -32,9 +66,9 @@ if __name__ == '__main__':
             break
         
         tokens = [token for token in request.strip().split(" ") if token != "AND"]
-        result = search_request(tokens, index)
-        # result = list(itertools.islice(search_request(tokens, index), 5))
-        if(result == []):
+        results = search_request(tokens, index)[:5]
+        
+        if(results == []):
             print("No results found!")
         else:
-            print(result)
+            print(results)
